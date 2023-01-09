a9ebe00ab93408be7739105bf1830f54b385077a
Amit-Swain <amitswain001@gmail.com>
2023-01-09 12:03:07 +0530

    First Commit

---
+++ DemoApp/src/in/ineuron/main/DemoApp.java
@@ -0,0 +1,18 @@
+package in.ineuron.main;
+
+public class DemoApp {
+
+	public static void main(String[] args) {
+		
+		int[] a = {10,20,30,40};
+		
+		for(int elem : a) {
+			System.out.println(elem);
+		}
+		
+		String name = "sachin";
+		int count = name.toUpperCase().length();
+		System.out.println("The length of the string is :: "+count);
+	}	
+
+}
